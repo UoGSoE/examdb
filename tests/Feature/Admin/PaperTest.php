67c1eda859b28888db5e65425c1052db8b871ef1
Billy <william.allan@glasgow.ac.uk>
2018-11-07 14:51:25 +0000

    Stub test for the papers index page

---
+++ tests/Feature/Admin/PaperTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PaperTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function admins_can_see_all_the_paper_statuses_for_all_the_courses()
+    {
+        $this->fail('TODO');
+    }
+}
