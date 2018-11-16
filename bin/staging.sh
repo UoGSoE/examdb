6a9a29bd9712ab2191e0137967dba7aab952a4c9
Billy <william.allan@glasgow.ac.uk>
2018-11-16 14:56:13 +0000

    More work on a staging script

--- bin/staging.sh
+++ bin/staging.sh
@@ -11,4 +11,5 @@ then
 fi
 
 BRANCH=${BRANCH:-master}
-ssh billy@"${STAGING_SERVER}" APP_NAME=${APP_NAME} "cd docker-apps/$APP_NAME && git pull origin ${BRANCH} && docker-compose build"
+LOCAL_IP=`dscacheutil -q host -a name ${STAGING_SERVER} | grep ip_address | cut -f2 -d' '`
+ssh billy@"${STAGING_SERVER}" APP_NAME=${APP_NAME} LOCAL_IP=${LOCAL_IP} "cd docker-apps/$APP_NAME && git pull origin ${BRANCH} && docker-compose build && docker-compose push && docker stack deploy -c docker-compose.yml $APP_NAME"
