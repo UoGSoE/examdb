5c206586796e51b18e36ff53ffc7ed212ea5dac1
Laravel Shift <shift@laravelshift.com>
2020-09-18 11:48:54 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.

--- config/auth.php
+++ config/auth.php
@@ -68,8 +68,9 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Models\User::class,
         ],
+
         'ldapusers' => [
             'driver' => 'ldapeloquent',
             'model' => App\User::class,
