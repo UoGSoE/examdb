e459fb59632f3728af2fa026838c25f04e1786a4
Billy <william.allan@glasgow.ac.uk>
2020-10-12 15:27:13 +0100

    Changed wording on email to setter.  Fixes #81

--- resources/views/emails/moderator_has_updated_the_checklist.blade.php
+++ resources/views/emails/moderator_has_updated_the_checklist.blade.php
@@ -4,7 +4,7 @@
 Dear exam paper setter,
 
 The moderator has reviewed your exam paper for {{ $course->code }} and has uploaded comments.
-Please check the comments and make the adjustments to your papers as required.
+Please check the comments on the checklist and make the adjustments to your papers as required.
 Please remember to complete and upload the checklist as without the checklist, the moderation process will not be triggered.
 
 @component('mail::button', ['url' => route('course.show', $course->id)])
