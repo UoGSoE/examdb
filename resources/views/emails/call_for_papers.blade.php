92c8171bb9f91efb0af041547d5c9b484fbb23e4
Billy <william.allan@glasgow.ac.uk>
2020-10-19 10:51:29 +0100

    Updated ldap auth dep and fixed a route in a mailable

--- resources/views/emails/call_for_papers.blade.php
+++ resources/views/emails/call_for_papers.blade.php
@@ -4,7 +4,7 @@
 Dear exam paper question setter,  Please upload your exam paper for the upcoming exam diet and the resit paper by {{ $deadline->format('d/m/Y') }}.
 Please remember to complete and upload the checklist as without the checklist, the moderation process will not be triggered.
 
-@component('mail::button', ['url' => route('/')])
+@component('mail::button', ['url' => url('/')])
 Log in here
 @endcomponent
 
