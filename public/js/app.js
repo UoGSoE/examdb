4f7050e0f684bba6debfc06d907a3beb19098f78
Billy <billybofh@gmail.com>
2021-05-13 09:44:18 +0100

    Added download attrib to paper download link

--- public/js/app.js
+++ public/js/app.js
@@ -43165,7 +43165,12 @@ var render = function() {
                   paper.subcategory != "comment"
                     ? _c(
                         "a",
-                        { attrs: { href: _vm.getDownloadRoute(paper) } },
+                        {
+                          attrs: {
+                            href: _vm.getDownloadRoute(paper),
+                            download: ""
+                          }
+                        },
                         [
                           _c("strong", [
                             _vm._v(_vm._s(paper.original_filename))
