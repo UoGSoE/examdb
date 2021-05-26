74d151ad13468fef8c315df9f0ee524bcc12d7f4
Billy <billybofh@gmail.com>
2021-05-26 16:02:55 +0100

    More yaml :'-(

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+set -e -u -o pipefail
+
+if [ -z "$1" ]
+then
+    echo "You need to supply either 'qa' or 'production' as an argument"
+    exit 1
+fi
+
 kustomize edit set image the-app=${IMAGE_NAME}
 kustomize edit set namespace ${STACK_NAME}
+echo $DOTENV > .env
+kustomize build overlays/$1 | envsubst | tee deployed.yml
+rm -f .env
+# kubectl apply -f deployed.yml
