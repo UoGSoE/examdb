dee1a234a537a22a2e8f26a5718c1277a909c4a4
Billy <billybofh@gmail.com>
2021-05-27 14:31:01 +0100

    More k8s stuff

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e -u -o pipefail
+set -e -o pipefail
 
 if [ -z "$1" ]
 then
@@ -8,9 +8,15 @@ then
     exit 1
 fi
 
-kustomize edit set image the-app=${IMAGE_NAME}
-kustomize edit set namespace ${STACK_NAME}
-echo $DOTENV > .env
+set -u
+
+echo "Deploying to ${1} with :"
+echo "IMAGE_NAME: ${IMAGE_NAME}"
+echo "NAMESPACE: ${NAMESPACE}"
+echo "TRAEFIK_HOSTNAME: ${TRAEFIK_HOSTNAME}"
+echo
+
+echo "${DOTENV}" > .env
 kustomize build overlays/$1 | envsubst | tee deployed.yml
 rm -f .env
 # kubectl apply -f deployed.yml
