4fbfcfdda04b196da6e64e0ce18c6d2cef6fb676
Billy <billybofh@gmail.com>
2021-07-01 11:02:30 +0100

    Try out using docker-compose to run phpunit tests

--- test-compose.sh
+++ test-compose.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-IMAGE_NAME=examdb-test:latest
+#IMAGE_NAME=examdb-test:latest
 
-docker build -t ${IMAGE_NAME} .
+#docker build -t ${IMAGE_NAME} .
 
 docker-compose -f docker-compose.test.yml up --exit-code-from test
 
