7290313cd58d327c623ee7dd4589d055872d5847
Billy <billybofh@gmail.com>
2021-08-30 13:57:00 +0100

    Add an artisan command to create an admin account

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use App\AcademicSession;
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'examdb:createadmin {username} {email} {forename} {surname}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an admin account';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $session = AcademicSession::getDefault();
+        if (! $session) {
+            $session = AcademicSession::createFirstSession();
+        }
+
+        User::create([
+            'username' => $this->argument('username'),
+            'password' => bcrypt(Str::random(64)),
+            'email' => $this->argument('email'),
+            'surname' => $this->argument('surname'),
+            'forenames' => $this->argument('forename'),
+            'is_staff' => true,
+            'is_admin' => true,
+            'academic_session_id' => $session->id,
+        ]);
+
+        activity()->log('Created admin account '.$this->argument('username').' via the CLI');
+    }
+}
