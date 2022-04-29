b2aa208cfa55cb1261b39c6b69c25b7f6e8b2114
Shift <shift@laravelshift.com>
2022-04-29 10:16:59 +0000

    Adopt anonymous migrations

--- database/migrations/2020_01_10_121838_create_paper_checklists_table.php
+++ database/migrations/2020_01_10_121838_create_paper_checklists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaperChecklistsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -36,4 +36,4 @@ class CreatePaperChecklistsTable extends Migration
     {
         Schema::dropIfExists('paper_checklists');
     }
-}
+};
