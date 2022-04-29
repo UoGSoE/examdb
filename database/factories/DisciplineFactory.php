20d5b1b94257c7382c0ada3c0914e740dd5d5a10
Shift <shift@laravelshift.com>
2022-04-29 10:16:47 +0000

    Remove unnecessary `$model` property

--- database/factories/DisciplineFactory.php
+++ database/factories/DisciplineFactory.php
@@ -9,13 +9,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class DisciplineFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Discipline::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
