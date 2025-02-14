4fb9809e7ebc8192b2ffc6de7e94418989da792c
Shift <shift@laravelshift.com>
2025-02-14 13:12:58 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote');
+
+
+Schedule::command('activitylog:clean')->daily();
+Schedule::command('examdb:timed-notifications')->dailyAt('03:00');
+Schedule::command('examdb:send-print-ready-reminder-emails')->dailyAt('07:00');
